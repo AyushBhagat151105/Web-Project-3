c7e64d4810c77bea48c58073a810dab4b8cde62c
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-09 01:10:51 +0530

    yoo 90% complit

---
+++ script.js
@@ -0,0 +1,66 @@
+//locomotive js - smooth scrolling
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+//gsap to animate
+
+gsap.from(".nlink",{
+    stagger: .1,
+    y:20,
+    duration: 2,
+    ease: Power2,
+    opacity: 0
+})
+
+Shery.textAnimate("#headings h1" /* Element to target.*/, {
+    //Parameters are optional.
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: 1,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
+
+  gsap.from(".anim2",{
+    y: 30,
+    stagger: .3,
+    opacity: 0,
+    opacity: 0,
+    ease: Power4,
+    duration: 1
+  })
+
+//shery js to animate images
+
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    duration: 1,
+  });
+
+Shery.imageEffect("#imgetext img",{
+    style: 4,
+    config: {"uColor":{"value":false},"uSpeed":{"value":1.41,"range":[0.1,1],"rangep":[1,10]},"uAmplitude":{"value":1.95,"range":[0,5]},"uFrequency":{"value":4.89,"range":[0,10]},"geoVertex":{"range":[1,64],"value":22.16},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.75},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.64,"range":[1,5]},"scrollType":{"value":0},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+    
+})
+
+Shery.imageEffect(".imgff img",{
+    style: 2,
+    config:{"resolutionXY":{"value":100},"distortion":{"value":true},"mode":{"value":-2},"mousemove":{"value":0},"modeA":{"value":1},"modeN":{"value":0},"speed":{"value":1,"range":[-500,500],"rangep":[-10,10]},"frequency":{"value":50,"range":[-800,800],"rangep":[-50,50]},"angle":{"value":0.5,"range":[0,3.141592653589793]},"waveFactor":{"value":1.4,"range":[-3,3]},"color":{"value":10212607},"pixelStrength":{"value":3,"range":[-20,100],"rangep":[-20,20]},"quality":{"value":5,"range":[0,10]},"contrast":{"value":1,"range":[-25,25]},"brightness":{"value":1,"range":[-1,25]},"colorExposer":{"value":0.18,"range":[-5,5]},"strength":{"value":0.2,"range":[-40,40],"rangep":[-5,5]},"exposer":{"value":8,"range":[-100,100]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.6666930129623775},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.58,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+})
+
+gsap.from("#imgetext img",{
+    y: "70",
+    opacity: 0,
+    duration: 2,
+    ease: expo,
+});
+
+Shery.imageEffect("#bimg",{
+    style: 5,
+    gooey: true,
+    debug: true,
+});
