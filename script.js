ae9ff39c247a5d117b002085a872493abe20670d
AyushBhagat <ayushnbhagat151105@gmail.com>
2024-02-09 14:04:03 +0530

    project ends

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//locomotive js - smooth scrolling
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
@@ -56,11 +55,29 @@ gsap.from("#imgetext img",{
     y: "70",
     opacity: 0,
     duration: 2,
-    ease: expo,
 });
 
-Shery.imageEffect("#bimg",{
-    style: 5,
+Shery.imageEffect("#banner .bimg", {
+    style: 6,
+    config: {"noiseDetail":{"value":7.44,"range":[0,100]},"distortionAmount":{"value":2.98,"range":[0,10]},"scale":{"value":36.36,"range":[0,100]},"speed":{"value":0.79,"range":[0,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.2102717084576313},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.23,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":35}},"discard_threshold":{"value":0.73,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.41,"range":[0,2]},"noise_scale":{"value":56.49,"range":[0,100]}},
     gooey: true,
-    debug: true,
-});
+  });
+
+  document.querySelector("#future button")
+  .addEventListener("mouseover", function(){
+    gsap.to("#future video",{
+        opacity: 1,
+        duration: 1.5,
+        ease: Power2
+    })
+
+  })
+
+  document.querySelector("#future button")
+  .addEventListener("mouseleave", function(){
+    gsap.to("#future video",{
+        opacity: 0,
+        duration: 1.5,
+        ease: Power2
+    })
+  })
